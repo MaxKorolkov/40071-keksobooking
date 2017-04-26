541fc95b414ab54282afbbeda284ad4f24075bd4
Maxim Korolkov <maximkorolkov1@gmail.com>
2017-04-26 03:03:01 +0300

    Second commit

--- js/map.js
+++ js/map.js
@@ -1,6 +1,19 @@
 'use strict';
 
+(function () {
 
+  // переменные для блока меток
+  var pinMap = document.querySelector('.tokyo__pin-map');
+
+  // создание и добавление меток чероз DocumentFragment
+  var pinFragment = document.createDocumentFragment();
+  for (var i = 0; i < offers.length; i++) {
+    /*  pinFragment.appendChild(window.pin.renderPin(window.offers[i]));*/
+    pinFragment.appendChild(pinAction.renderPin(offers[i]));
+  }
+  pinMap.appendChild(pinFragment);
+
+})();
 
 
 
--- js/pin.js
+++ js/pin.js
@@ -1,47 +1,46 @@
 'use strict';
 
-// переменные для блока меток
-var pin = document.querySelector('.pin');
-var pinMap = document.querySelector('.tokyo__pin-map');
-
-// функция удаления активной метки
-function removePinActive() {
-  var pinActive = document.querySelector('.pin--active');
-  if (pinActive) {
-    pinActive.classList.remove('pin--active');
-  }
-}
+// Глобальный объект для создания метки и обрабоки событий
+  window.pinAction = (function () {
+    // функция удаления активной метки
+    var removePinActive = function() {
+      var pinActive = document.querySelector('.pin--active');
+      if (pinActive) {
+        pinActive.classList.remove('pin--active');
+      }
+    };
 
 // функция генерации активной метки
-function addPinActive(pinElement) {
-  removePinActive();
-  pinElement.classList.add('pin--active');
-}
+    var addPinActive = function(pinElement) {
+      removePinActive();
+      pinElement.classList.add('pin--active');
+    };
 
-// генерация метки и обработка клика и нажатия Enter при фокусе
-function renderPin(offer) {
-  var pinElement = pin.cloneNode(true);
-  pinElement.style.left = offer.location.x - 37 + 'px';
-  pinElement.style.top = offer.location.y - 92 + 'px';
-  pinElement.querySelector('img').src = offer.author.avatar;
-  pinElement.addEventListener('click', function () {
-    addPinActive(pinElement);
-    showDialog(offer);
-  });
-  pinElement.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-      addPinActive(pinElement);
-      showDialog(offer);
-    }
-  });
-  return pinElement;
-}
+    // генерация метки и обработка клика и нажатия Enter при фокусе
+    var renderPin = function (offer) {
+      var pin = document.querySelector('.pin');
+      var pinElement = pin.cloneNode(true);
+      pinElement.style.left = offer.location.x - 37 + 'px';
+      pinElement.style.top = offer.location.y - 92 + 'px';
+      pinElement.querySelector('img').src = offer.author.avatar;
+      pinElement.addEventListener('click', function () {
+        addPinActive(pinElement);
+        renderDialog.showDialog(offer);
+      });
+      pinElement.addEventListener('keydown', function (event) {
+        if (event.keyCode === 13) {
+          addPinActive(pinElement);
+          renderDialog.showDialog(offer);
+        }
+      });
+      return pinElement;
+    };
 
-// создание и добавление меток чероз DocumentFragment
-var pinFragment = document.createDocumentFragment();
-for (var i = 0; i < offers.length; i++) {
-  pinFragment.appendChild(renderPin(offers[i]));
-}
-pinMap.appendChild(pinFragment);
+    return {
+      renderPin: renderPin,
+      removePinActive: removePinActive
+    };
+  })();
+
 
 
